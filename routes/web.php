9ef5b00af4a06063712151e0c02ff649f7015f55
Jaman <ravshanovsamir@gmail.com>
2022-04-05 15:30:56 +1100

    update web

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
  
 Route::get('/', function () {
-    return view('welcome', ['say' => 'hello']);
+    return view('welcome');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
